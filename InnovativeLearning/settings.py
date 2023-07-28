56a9251d65dca67a0482522ea6398d0a6e03b506
Govind786 <govindkushwaha0786@gmail.com>
2023-07-28 11:43:33 +0530

    added settings

--- InnovativeLearning/settings.py
+++ InnovativeLearning/settings.py
@@ -111,6 +111,10 @@ USE_TZ = True
 MEDIA_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 
+if not DEBUG:
+    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 # user_image manage
 MEDIA_ROOT = os.path.join(BASE_DIR, 'display_pictures')
 MEDIA_URL = '/display_pictures/'
